68f34d4d1fce8e7248c9b5c7681bbb98bf6d96e5
Arthur Grillo <arthur.grillo@usp.br>
2021-05-09 21:47:54 -0300

    optmized the code with Newton's root finder algorithm

--- ICC-33.py
+++ ICC-33.py
@@ -1,12 +1,22 @@
 numero = int(input())
 
-res = 0
-for i in range(2,numero):
-    if numero / (i * (i + 1)*(i+2)) == 1:
-        res = i
-        break
+def func(x,n):
+    return x * x * x + 3 * x * x + 2 * x - n
 
-if res != 0:
+def dfunc(x):
+    return 3 * x * x + 6 * x + 2
+
+def consecutiveMultiplicaton(n):
+    pchute = 0
+    chute = n ** (1/3)
+
+    while round(pchute,5) != round(chute,5):
+        pchute = chute
+        chute = chute - func(chute,n)/dfunc(chute)
+    return round(chute,7)
+
+n = consecutiveMultiplicaton(numero)
+if n // 1 == n:
     print("S")
 else:
     print("N")
