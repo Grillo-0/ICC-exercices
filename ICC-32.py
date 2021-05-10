ab46bb1667b1c93f75d2b626cf6a49888b53fb6c
Arthur Grillo <arthur.grillo@usp.br>
2021-05-09 22:42:23 -0300

    corrected the exercise to get 10/10

--- ICC-32.py
+++ ICC-32.py
@@ -7,19 +7,23 @@ for i in range(n):
 pri = conjunto[1]
 seg = conjunto[0]
 estado = 0
+adicional = 0
 
-for i in range(2,n):
-    if seg <= pri:
-        estado = estado + 1
+for i in range(n-1):
+    if seg < pri:
+        estado = estado + 5
+    elif seg == pri:
+        adicional = adicional + 1
     else:
         estado =  estado - 1
-    seg = pri
-    pri = conjunto[i]
+    if i + 2 != n:
+        seg = pri
+        pri = conjunto[i + 2]
 
-if estado == n - 2:
+if estado == 5 * (n - 1 - adicional):
     print(1)
 else:
-    if estado == -n + 2:
+    if estado == -n + 1 + adicional:
         print(-1)
     else:
         print(0)
